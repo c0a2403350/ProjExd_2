11520e5837c771469c0ab7ac55828e35687933d5
Impas <c0a2403350@edu.teu.ac.jp>
2025-11-25 14:13:40 +0900

    爆弾実装

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -1,7 +1,7 @@
 import os
 import sys
+import random
 import pygame as pg
-
 
 WIDTH, HEIGHT = 1100, 650
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
@@ -13,14 +13,26 @@ MOVEMENT = {
     pg.K_RIGHT: (5, 0),
 }
 
-
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
     screen = pg.display.set_mode((WIDTH, HEIGHT))
+
+    #背景
     bg_img = pg.image.load("fig/pg_bg.jpg")    
+
+    #こうかとん
     kk_img = pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 0.9)
     kk_rct = kk_img.get_rect()
     kk_rct.center = 300, 200
+    
+    #爆弾
+    bb_img = pg.Surface((20, 20))
+    bb_img.set_colorkey((0,0,0))
+    pg.draw.circle(bb_img, (255, 0, 0), (10, 10), 10)
+    bb_rct = bb_img.get_rect()
+    bb_rct.center = random.randint(0, WIDTH), random.randint(0, HEIGHT)
+    vx,  vy = 5, 5
+    
     clock = pg.time.Clock()
     tmr = 0
     
@@ -30,15 +42,20 @@ def main():
                 return
         screen.blit(bg_img, [0, 0]) 
 
+        #こうかとん
         key_lst = pg.key.get_pressed()
         sum_mv = [0, 0]
         for key, mv in MOVEMENT.items():
             if key_lst[key]:
                 sum_mv[0] += mv[0]
                 sum_mv[1] += mv[1]
-                
         kk_rct.move_ip(sum_mv)
         screen.blit(kk_img, kk_rct)
+
+        #爆弾
+        screen.blit(bb_img, bb_rct)
+        bb_rct.move_ip(vx, vy)
+        
         pg.display.update()
         tmr += 1
         clock.tick(50)
