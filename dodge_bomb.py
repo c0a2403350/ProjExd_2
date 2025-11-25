2631c91760b003edf3543c601f8abed4b654266b
Impas <c0a2403350@edu.teu.ac.jp>
2025-11-25 14:37:15 +0900

    練習3

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -4,14 +4,32 @@ import random
 import pygame as pg
 
 WIDTH, HEIGHT = 1100, 650
-os.chdir(os.path.dirname(os.path.abspath(__file__)))
-
 MOVEMENT = {
     pg.K_UP: (0, -5),
     pg.K_DOWN: (0, 5),
     pg.K_LEFT: (-5, 0),
     pg.K_RIGHT: (5, 0),
 }
+
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+"""
+check_bound関数
+引数：オブジェクトRect
+戻り値：（横軸のはみ出しbool判定, 縦軸のはみ出しbool判定）
+
+画面内なら1, 画面外なら0を返す
+"""
+def check_bound(obj_rct: pg.Rect) -> tuple[bool, bool]:
+    x, y = 1, 1
+    if obj_rct.left < 0 or obj_rct.right > WIDTH:
+        x = 0
+    if obj_rct.top < 0 or obj_rct.bottom > HEIGHT:
+        y = 0
+        
+    return (x, y)
+       
+        
 
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
@@ -42,18 +60,28 @@ def main():
                 return
         screen.blit(bg_img, [0, 0]) 
 
-        #こうかとん
         key_lst = pg.key.get_pressed()
+        
+        #こうかとん
         sum_mv = [0, 0]
         for key, mv in MOVEMENT.items():
             if key_lst[key]:
                 sum_mv[0] += mv[0]
                 sum_mv[1] += mv[1]
+        if check_bound(kk_rct) != (True, True):
+            sum_mv[0] *= -1
+            sum_mv[1] *= -1
         kk_rct.move_ip(sum_mv)
         screen.blit(kk_img, kk_rct)
 
         #爆弾
         screen.blit(bb_img, bb_rct)
+        jdg = check_bound(bb_rct)
+        if jdg != (True, True):
+            if not jdg[0]:
+                vx *= -1
+            if not jdg[1]:
+                vy *= -1
         bb_rct.move_ip(vx, vy)
         
         pg.display.update()
