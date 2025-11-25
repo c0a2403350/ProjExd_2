1258d3bc6809f894485bb5d0a6056f804703d84f
Impas <c0a2403350@edu.teu.ac.jp>
2025-11-25 14:43:41 +0900

    練習4

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -58,6 +58,10 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT: 
                 return
+        if kk_rct.colliderect(bb_rct):
+            print("GameOver")
+            return
+            
         screen.blit(bg_img, [0, 0]) 
 
         key_lst = pg.key.get_pressed()
@@ -86,8 +90,7 @@ def main():
         pg.display.update()
         tmr += 1
         clock.tick(50)
-
-
+        
 if __name__ == "__main__":
     pg.init()
     main()
